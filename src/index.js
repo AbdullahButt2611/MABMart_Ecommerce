6835c2a71296116af220324b8d1098353638b840
M. Abdullah Butt <81871162+AbdullahButt2611@users.noreply.github.com>
2023-08-27 15:01:15 +0500

    Basic Layout
    Started Working on the Header Section.

--- src/index.js
+++ src/index.js
@@ -5,9 +5,13 @@ import "bootstrap/dist/css/bootstrap.css";
 
 import App from "./App";
 
+import { BrowserRouter } from "react-router-dom";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
